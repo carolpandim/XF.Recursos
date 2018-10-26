a1aafde165b1d55eb133da954f99fca35a81e432
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-25 23:05:23 -0300

    Add project files.

---
+++ XF.Recursos/XF.Recursos/Menu/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.App_Code;
+
+namespace XF.Recursos.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+            menuPage.ListaMenu.ItemSelected += ListaMenu_ItemSelected;
+		}
+
+        private void ListaMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as OpcoesMenu;
+
+            if (item!=null)
+            {
+                Detail = new NavigationPage(((Page)Activator.CreateInstance(item.TargetType)));
+                menuPage.ListaMenu.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
---
+++ XF.Recursos/XF.Recursos/Menu/MenuView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.App_Code;
+
+namespace XF.Recursos.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuView : ContentPage
+	{
+        public ListView ListaMenu { get { return lstMenu; } }
+		public MenuView ()
+		{
+			InitializeComponent ();
+            InicializarMenu();
+		}
+
+        private void InicializarMenu()
+        {
+            ObservableCollection<OpcoesMenu> menuItens = new ObservableCollection<OpcoesMenu>();
+
+            menuItens.Add(new OpcoesMenu()
+            {
+                Descricao = "Home",
+                Icone = "",
+                TargetType = typeof(HomeView)
+
+            });
+
+            lstMenu.ItemsSource = menuItens;
+        }
+    }
+}
